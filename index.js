03ac2208234736c55917e3968449b7cfac0410f1
acarmena <camond87979@uat.edu>
2026-07-14 08:20:55 -0600

    Refactor index.js to include student object and DOM updates
    Updated functions to manipulate DOM elements and added a student object.

--- index.js
+++ index.js
@@ -3,27 +3,31 @@
 
 const school = "the University of Advancing Technology";
 
+const student = {
+    name: "Carmen Amond",
+    major: "Computer Science",
+    email: "carmen.amond@university.edu",
+    gradYear: 2028
+};
 
 function showGreeting(userName){
+    document.getElementById("userName").textContent = userName;
     console.log("Hello " + userName + "!");
     console.log("Welcome to " + school);
 }
 
 function info(major, email){
+    document.getElementById("major").textContent = major;
+    document.getElementById("email").textContent = email;
     console.log("Your major is " + major);
     console.log("Your school email is " + email);
 }
 
 function gradTime(gradYear){
+    document.getElementById("gradYear").textContent = gradYear;
     console.log("You will graduate in " + gradYear);
 }
 
 showGreeting("Carmen");
 info("Computer Science", "carmen.amond@university.edu");
 gradTime(2028);
-showGreeting("Aiden");
-info("Mathematics", "aiden.cassidy@university.edu");
-gradTime(2032);
-showGreeting("Lincoln");
-info("Physics", "lincoln.willison@university.edu");
-gradTime(2029);
